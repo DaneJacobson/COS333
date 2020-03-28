30efaaf74de3b56f5d2b6de0c06b00481b9cd07c
Dane Jacobson <danej@princeton.edu>
2020-03-28 02:59:22 -0400

    deleted sum shit

--- A3/registrar.py
+++ A3/registrar.py
@@ -21,11 +21,13 @@ app = Flask(__name__, template_folder='.')
 @app.route('/index', methods=['GET'])
 def index():
 
+    # generate search query
     dept = request.args.get('dept')
     coursenum = request.args.get('coursenum')
     area = request.args.get('area')
     title = request.args.get('title')
 
+    # if this is the first page load, set fields to ''
     if dept is None:
         dept = ''
     if coursenum is None:
@@ -35,22 +37,23 @@ def index():
     if title is None:
         title = ''
 
+    # build query and perform search
     query = {'-dept':dept,'-coursenum':coursenum, '-area':area, '-title':title}
-
     database = Database()
     entries = database.search(query)
 
+    # error handling, create entries for template
     if 'error' in entries:
         return redirect(url_for('error', errorMsg=entries.get('error')))
     entries = entries.get('success')
 
+    # render index.html, set cookies for fields, and return
     html = render_template('index.html',
         dept=dept,
         coursenum=coursenum,
         area=area,
         title=title,
         entries=entries)
-
     response = make_response(html)
     response.set_cookie('dept', dept)
     response.set_cookie('coursenum', coursenum)
@@ -63,21 +66,24 @@ def index():
 @app.route('/regdetails', methods=['GET'])
 def regdetails():
     
+    # fetch cookie fields
     dept = request.cookies.get('dept')
     coursenum = request.cookies.get('coursenum')
     area = request.cookies.get('area')
     title = request.cookies.get('title')
-
     classid = request.args.get('classid')
+    
+    # build details query and perform search
     classid_query = {'classid':classid}
-
     database = Database()
     details = database.get_details(classid_query)
 
+    # error handling, create details for template
     if 'error' in details:
         return redirect(url_for('error', errorMsg=details.get('error')))
     details = details.get('success')
 
+    # render regdetails.html, and return
     html = render_template('regdetails.html',
         classid=classid,
         dept=dept,
@@ -93,6 +99,7 @@ def regdetails():
 @app.route('/error')
 def error():
 
+    # render error.html with application-specific message, and return
     html = render_template('error.html', 
         errorMsg=request.args.get('errorMsg'))
     response = make_response(html)
